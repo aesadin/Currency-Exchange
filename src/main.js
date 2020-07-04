66a35bfbbf365a50f8d9cde6bb4da9c956d255ee
aesadin <65793229+aesadin@users.noreply.github.com>
2020-07-03 17:18:56 -0700

    Update main.js
    Adding my comments, for some reason they were pushed without saving

--- src/main.js
+++ src/main.js
@@ -4,32 +4,38 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles.css';
 import { callExchangeRate } from './src/currencyexchange.js';
 
-
+// returns error message to user
 async function getExchangeRate() {
   const allExchangeRates = await callExchangeRate();
   if(!allExchangeRates) {
     return 'There has been an error processing your request'
   } else {
-    showExchangeRate();
+    showExchangeRate(allExchangeRates.conversion_rates);
   }
 }
 
-function showExchangeRate(currency) {
-  let currency = $("#currency").val("");
-  if(currency === "EUR" ) {
+// displays user inputted currency or tells user currency is not available
+function showExchangeRate(currencyType) {
+  if(currencyType === "EUR" ) {
     $('.revealedRate').text(`Your converted currency is ${exchangeResponse.conversion_rates.EUR}`);
-  } else if(currency === "NZD") {
+  } else if(currencyType === "NZD") {
     $('.revealedRate').text(`Your converted currency is ${exchangeResponse.conversion_rates.NZD}`);
-  } else if(currency === "NOK") {
+  } else if(currencyType === "NOK") {
     $('.revealedRate').text(`Your converted currency is ${exchangeResponse.conversion_rates.NOK}`);
-  } else if(currency === "PLN") {
+  } else if(currencyType === "PLN") {
     $('.revealedRate').text(`Your converted currency is ${exchangeResponse.conversion_rates.PLN}`);
-  } else if(currency === "SAR") {
+  } else if(currencyType === "SAR") {
     $('.revealedRate').text(`Your converted currency is ${exchangeResponse.conversion_rates.SAR}`);
   } else {
-    $('.revealedCurrency').text('This currency is not available');
+    $('.revealedRate').text('This currency is not available');
   }
-  
+  console.log(currencyType)
+}
+
+// still working to make this a proper UI function, but it will calculate the new currency in the user's preferred country
+function calculateCurrencyOutput(usdCurrency, exchangeRate) {
+  let newCurrency = (usdCurrency * exchangeRate);
+  return newCurrency;
 }
 
 
@@ -40,10 +46,11 @@ $(document).ready(function () {
     let usMoney = parseInt($("#exchange").val(""));
     let currencyType = $("#currency").val("").toUpperCase();
     callExchangeRate();
+    getExchangeRate()
     showExchangeRate(currencyType);
 
     console.log(usMoney)
-    $(".revealedRate").html();
+    $(".exchangedCurrency").html();
 
   });
 });
