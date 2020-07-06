feed5d0f910f8c736151b4478d9e61c80bf6454c
aesadin <65793229+aesadin@users.noreply.github.com>
2020-07-06 08:01:41 -0700

    Update currencyexchange.js

--- src/currencyexchange.js
+++ src/currencyexchange.js
@@ -1,7 +1,7 @@
 
 export async function callExchageRate() {
   try {
-    let response = await fetch(`https://v6.exchangerate-api.com/v6/latest/USD?appid=${process.env.API_KEY}`); 
+    let response = await fetch(`https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/latest/USD?base-code=USD&appid=${process.env.API_KEY}`); 
     let jsonifiedResponse;
     if (response.ok && response.status == 200) {
       jsonifiedResponse = await response.json();
